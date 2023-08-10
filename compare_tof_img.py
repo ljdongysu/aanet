29b211e4f2cf2ce360508b019722333ab2fa1ab5
donglijian@indemind.cn <donglijian@indemind.cn>
2023-08-10 15:09:54 +0800

    use depth image is not 0 to compare with tof

--- compare_tof_img.py
+++ compare_tof_img.py
@@ -26,16 +26,26 @@ def Print_distance(depth, tof, image=None):
         image_with_tof_box = cv2.imread(image)
     image_depth = cv2.imread(depth)
     image_tof = cv2.imread(tof)
-    image_tof = image_tof[:, :, 0] + (image_tof[:, :, 1] > 0) * 255 + image_tof[:, :, 1] + (image_tof[:, :, 2] > 0) * 511 + image_tof[:, :, 2]
-    image_tof_with_value = image_tof.copy()
-    image_tof_with_value[image_tof > 0] = 1
+
+    # 深度估计图像获取非0的index
     image_depth = image_depth[:, :, 0] + (image_depth[:, :, 1] > 0) * 255 + image_depth[:, :, 1] + (image_depth[:, :, 2] > 0) * 511 + image_depth[:, :, 2]
+    image_depth_with_value = image_depth.copy()
+    image_depth_with_value[image_depth_with_value > 0] = 1
+
+    image_tof = image_tof[:, :, 0] + (image_tof[:, :, 1] > 0) * 255 + image_tof[:, :, 1] + (image_tof[:, :, 2] > 0) * 511 + image_tof[:, :, 2]
+    # 仅计算深度估计非0位置
+    image_tof = image_tof * image_depth_with_value
+
+    image_tof_with_value = image_tof.copy()
+
+    image_tof_with_value[image_tof > 0] = 1
+
     if image is None:
         image_depth_tof = np.abs(image_depth * image_tof_with_value - image_tof)
     else:
         image_box = (image_with_tof_box[:, :, 0] == 0) * (image_with_tof_box[:, :, 1] == 0) * (image_with_tof_box[:, :, 2] == 255) * image_tof_with_value
         image_depth_tof = np.abs(image_depth * image_box - image_tof * image_box)
-    print("选中的tof点数量: {}，全部tof点数量： {}".format(np.sum(image_box), np.sum(image_tof_with_value)))
+    print("选中的tof点数量: {}".format(np.sum(image_box)))
     print("选中的tof点中值: ", np.median(image_tof[image_box > 0]))
     print("深度估计与选中tof点的平均误差: {:.2}".format(np.sum(image_depth_tof) / np.sum(image_box)))
     print("深度点与选中tof点最大误差: ", np.max(image_depth_tof))
@@ -63,37 +73,11 @@ def Print_distance(depth, tof, image=None):
 
     plt.show()
 
-
     print("误差/真实距离 < 1%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception < 0.01), np.sum(perception < 0.01) / np.sum(image_box)))
     print("误差/真实距离 > 2%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception > 0.02), np.sum(perception > 0.02) / np.sum(image_box)))
     print("误差/真实距离 > 5%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception > 0.05) , np.sum(perception > 0.05) / np.sum(image_box)))
     image_depth_tof[image_depth_tof>0] = image_depth_tof[image_depth_tof>0] + 100
     cv2.imwrite("image_depth_tof.png", GetDepthImg(image_depth_tof))
-
-    # print("\n===================================== ")
-    # print("reduce 2.5 before abs:")
-    # print("===================================== \n")
-    # image_depth_tof = np.abs(image_depth * image_box - image_tof * image_box + image_box * 2.5)
-    # print("选中的tof点数量: {}，全部tof点数量： {}".format(np.sum(image_box), np.sum(image_tof_with_value)))
-    # print("选中的tof点中值: ", np.median(image_tof[image_box > 0]))
-    # print("深度估计与选中tof点的平均误差: {}".format(np.sum(image_depth_tof) / np.sum(image_box)))
-    # print("深度点与选中tof点最大误差: ", np.max(image_depth_tof))
-    # print("误差 > 2cm 点的数量： {}, 比例： {:.2%} ".format(np.sum(image_depth_tof > 2),np.sum(image_depth_tof > 2)/np.sum(image_box)))
-    # print("误差 > 5cm 点的数量： {}, 比例： {:.2%} ".format(np.sum(image_depth_tof > 5),np.sum(image_depth_tof > 5)/np.sum(image_box)))
-    # perception = image_depth_tof[image_depth_tof > 0] / (image_tof * image_box)[image_depth_tof > 0]
-    #
-    # print("误差比例平均值: {:.2%}".format(np.mean(perception)))
-    #
-    # plt.hist(perception, bins=int(np.max(perception) * 100))
-    # plt.show()
-    #
-    # print("误差/真实距离 < 1%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception < 0.01), np.sum(perception < 0.01) / np.sum(image_box)))
-    # print("误差/真实距离 > 2%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception > 0.02), np.sum(perception > 0.02) / np.sum(image_box)))
-    # print("误差/真实距离 > 5%的点数数量: {}, 比例： {:.2%}".format(np.sum(perception > 0.05) , np.sum(perception > 0.05) / np.sum(image_box)))
-    # image_depth_tof[image_depth_tof>0] = image_depth_tof[image_depth_tof>0] + 100
-    # cv2.imwrite("image_depth_tof_2.5.png", GetDepthImg(image_depth_tof))
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
